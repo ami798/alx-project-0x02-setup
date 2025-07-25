da3a83aed2bd964e7c067c7a0a74e8e646ead716
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-25 13:43:45 +0300

    addition of the button tsx

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,19 @@
+// components/common/Button.tsx
+import { type ButtonProps } from "@/interfaces";
+
+const sizeClasses = {
+  small: "px-3 py-1 text-sm",
+  medium: "px-4 py-2 text-base",
+  large: "px-5 py-3 text-lg",
+};
+
+export default function Button({ size, shape, children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-blue-600 text-white ${sizeClasses[size]} ${shape} hover:bg-blue-700 transition duration-300`}
+    >
+      {children}
+    </button>
+  );
+}
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+// pages/about.tsx
+import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
-const AboutPage = () => {
+export default function About() {
   return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold text-green-600">About Us</h1>
+    <div>
+      <Header />
+      <main className="p-6 space-y-4">
+        <h1 className="text-3xl font-bold mb-4">About Page</h1>
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <Button size="small" shape="rounded-sm">Small Button</Button>
+          <Button size="medium" shape="rounded-md">Medium Button</Button>
+          <Button size="large" shape="rounded-full">Large Button</Button>
+        </div>
+      </main>
     </div>
   );
-};
-
-export default AboutPage;
+}
